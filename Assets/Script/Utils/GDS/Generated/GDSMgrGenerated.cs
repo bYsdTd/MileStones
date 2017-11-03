a4db834e9c7c622a0c38ae7608630c6a46940c8e
zhangyi <nickzy@163.com>
2017-11-03 14:51:36 +0800

    删掉无用的gds

--- Assets/Script/Utils/GDS/Generated/GDSMgrGenerated.cs
+++ Assets/Script/Utils/GDS/Generated/GDSMgrGenerated.cs
@@ -6,10 +6,8 @@ namespace GDSKit
 {
 	public partial class GDSMgr
 	{
-		private string[] gdsFiles = new string[3]
+		private string[] gdsFiles = new string[1]
 		{
-			"parameter.csv",
-			"shop.csv",
 			"unit.csv",
 			
 		};
@@ -18,19 +16,7 @@ namespace GDSKit
 		{
 			// SLua.LuaFunction func = LuaGameManager.instance().GetState().getFunction("gdsInitCallback");
 
-			string parameter_data = GetGDSFileData(gdsFiles[0]);
-			if (!String.IsNullOrEmpty(parameter_data))
-			{
-				parameter.Initialize(CSVParser.Parse(parameter_data, CSVParser.PARSE_DATA).data);
-				// func.call("parameter", parameter_data);
-			}
-			string shop_data = GetGDSFileData(gdsFiles[1]);
-			if (!String.IsNullOrEmpty(shop_data))
-			{
-				shop.Initialize(CSVParser.Parse(shop_data, CSVParser.PARSE_DATA).data);
-				// func.call("shop", shop_data);
-			}
-			string unit_data = GetGDSFileData(gdsFiles[2]);
+			string unit_data = GetGDSFileData(gdsFiles[0]);
 			if (!String.IsNullOrEmpty(unit_data))
 			{
 				unit.Initialize(CSVParser.Parse(unit_data, CSVParser.PARSE_DATA).data);
@@ -39,14 +25,6 @@ namespace GDSKit
 			
 		}
 		
-		public parameter Getparameter(string parameter_name)
-		{
-			return parameter.GetInstance(parameter_name);
-		}
-		public shop Getshop(string item_name)
-		{
-			return shop.GetInstance(item_name);
-		}
 		public unit Getunit(string unit_name)
 		{
 			return unit.GetInstance(unit_name);
